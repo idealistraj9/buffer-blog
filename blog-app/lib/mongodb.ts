314f1101e43e4197d6c1818136a48d8e157dfa37
raj <d22it187@charusat.edu.in>
2024-05-30 14:53:59 +0530

    push

--- blog-app/lib/mongodb.ts
+++ blog-app/lib/mongodb.ts
@@ -1,32 +1,33 @@
-// lib/mongodb.ts
+// mongodb.ts
 
 import { MongoClient, MongoClientOptions } from 'mongodb';
 
 const uri: string = process.env.MONGODB_URI!;
-const options: MongoClientOptions = {
+const options: any = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 };
 
 let client: MongoClient | undefined;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
-  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Add Mongo URI to .env.local');
+  throw new Error('Add Mongo URI to.env.local');
 }
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    client = new MongoClient(uri, options); 
+    global._mongoClientPromise = client.connect(); 
   }
   clientPromise = global._mongoClientPromise!;
 } else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  client = new MongoClient(uri, options); 
+  clientPromise = client.connect(); 
 }
 
 export default clientPromise;
