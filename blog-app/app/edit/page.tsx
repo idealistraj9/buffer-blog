e56440fe61433ed1f89c49001234a366e391a729
raj <d22it187@charusat.edu.in>
2024-05-30 13:52:24 +0530

    push

--- blog-app/app/edit/page.tsx
+++ blog-app/app/edit/page.tsx
@@ -1,5 +1,5 @@
-'use client';
 // pages/edit-post/[id].tsx
+'use client';
 import { useState, useEffect, FormEvent } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
